3dc65547616b7a59d779dcd2c3c9acd2a3b0957b
kostaskotronis <kkostasdev@gmail.com>
2025-09-28 00:39:17 +0300

    let's finish our first entity

---
+++ backend/src/main/java/com/seizetheday/backend/domain/Role.java
@@ -0,0 +1,7 @@
+package com.seizetheday.backend.domain;
+
+public enum Role {
+    VIEWER,
+    CREATOR,
+    ADMIN
+}
--- backend/src/main/java/com/seizetheday/backend/domain/User.java
+++ backend/src/main/java/com/seizetheday/backend/domain/User.java
@@ -2,39 +2,54 @@ package com.seizetheday.backend.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
-@Table(name="user")
+@Table(name="users")
 @Getter
 @Setter
-
+@NoArgsConstructor // JPA needs a no-args constructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 50)
+    @Column(nullable=false, unique=true)
     private String username;
 
     @NotBlank
+    @Size(min = 8)
+    @Column(nullable=false)
     private String password;
 
-    @NotBlank
-    private String roles;
+    @NotEmpty
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
 
     @NotNull
-    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private Instant createdAt;
 
     @PrePersist
-    private void fillSystemFields() {
-        createdAt = LocalDateTime.now();
+    void onCreate() {
+        if (createdAt == null) {
+            createdAt = Instant.now(); // store UTC
+        }
     }
 
     @Override
@@ -42,11 +57,11 @@ public class User {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(roles, user.roles);
+        return Objects.equals(id, user.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, username, password, roles);
+        return Objects.hashCode(id);
     }
 }
