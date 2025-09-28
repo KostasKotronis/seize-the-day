2f144b7b8e50f9d8db665421d2865c82136ff254
kostaskotronis <kkostasdev@gmail.com>
2025-09-28 16:46:06 +0300

    initialize repositories

--- backend/src/main/java/com/seizetheday/backend/repository/AddressRepository.java
+++ backend/src/main/java/com/seizetheday/backend/repository/AddressRepository.java
@@ -2,7 +2,6 @@ package com.seizetheday.backend.repository;
 
 import com.seizetheday.backend.domain.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
--- backend/src/main/java/com/seizetheday/backend/repository/ReviewRepository.java
+++ backend/src/main/java/com/seizetheday/backend/repository/ReviewRepository.java
@@ -2,7 +2,6 @@ package com.seizetheday.backend.repository;
 
 import com.seizetheday.backend.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
